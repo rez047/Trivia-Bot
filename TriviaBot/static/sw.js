b9942a1e239c671e63863c9af9db58f34e3014b2
rez047 <113913401+rez047@users.noreply.github.com>
2026-07-01 02:55:05 +1000

    Create sw.js

---
+++ TriviaBot/static/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "trivia-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/static/icon-192.png",
+  "/static/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
